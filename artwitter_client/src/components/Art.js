a81a9dccf2fea64a962bccbd0fa42a7540a13fb5
yuliyambg <yuliyambg@gmail.com>
2021-05-12 20:07:18 -0400

    adding Art component

---
+++ artwitter_client/src/components/Art.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+import {connect} from "react-redux";
+import {addArt, fetchArt, fetchArts} from "../actions/artsActions";
+
+class Art extends React.Component {
+
+    componentWillMount() {
+        this.props.fetchArt(this.props.artId);
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>{this.props.artwork.title}</h1>
+                <h2>{this.props.artwork.artist_name}</h2>
+                <img src={this.props.artwork.image_url} />
+            </div>
+        )
+    }
+}
+
+const mapStateToPropsArt = state => {
+    return { artwork: state.arts }
+}
+
+export default connect(mapStateToPropsArt, {fetchArt})(Art);
