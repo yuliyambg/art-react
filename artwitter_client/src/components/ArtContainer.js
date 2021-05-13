64bbfef4cd7dd06dec3a75708e655b7d04474e8a
yuliyambg <yuliyambg@gmail.com>
2021-05-12 20:06:10 -0400

    adding ArtContainer

---
+++ artwitter_client/src/components/ArtContainer.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+import Art from "./Art";
+import CommentForm from "./CommentForm";
+import CommentList from "./CommentList";
+import {connect} from "react-redux";
+import {fetchComments} from "../actions/commentsAction";
+
+
+class ArtContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchComments(this.props.match.params.id)
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Art artId={this.props.match.params.id}/>
+                <hr/>
+                <CommentForm artId={this.props.match.params.id}/>
+                <hr/>
+                <CommentList />
+            </div>
+        )
+    }
+}
+
+export default connect(null, {fetchComments})(ArtContainer);
