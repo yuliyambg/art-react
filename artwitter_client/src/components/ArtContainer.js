07f962c93525412db107e0c30b666daa6c7e46de
yuliyambg <yuliyambg@gmail.com>
2021-05-13 21:05:59 -0400

    updating Home page

--- artwitter_client/src/components/ArtContainer.js
+++ artwitter_client/src/components/ArtContainer.js
@@ -8,14 +8,17 @@ import {fetchComments} from "../actions/commentsAction";
 
 class ArtContainer extends Component {
 
+
     componentDidMount() {
         this.props.fetchComments(this.props.match.params.id)
     }
 
 
+
     render() {
         return (
             <div>
+                <a href={`/arts`}>Go back</a>
                 <Art artId={this.props.match.params.id}/>
                 <hr/>
                 <CommentForm artId={this.props.match.params.id}/>
--- artwitter_client/src/components/Home.js
+++ artwitter_client/src/components/Home.js
@@ -1,11 +1,19 @@
 import React from "react";
+import styles from '../styles/styles.module.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const Home = () => {
     return(
-        <div>
-            Home Page
+        <div className={styles.about}>
+            <div className={styles.aboutContainer}>
+                <h1>FOR THE LOVE OF ART</h1>
+                <p>
+                    <span>Artwitter envisions a future where people are moved by art every day. It was built to enable people to meet on the platform and discuss about art. The platform also offers an opportunity for artists from all ages to share their artworks and gain popularity. Living in ArtWorld around the world. Discover new trends, new talents and simply enjoy fine art, we at Artwitter believe that sharing our passion for art will make the world a wonderful place as wonderful as the art itself. We are dedicated to making an exciting, welcoming experience that connects art-lovers, connoisseurs, artists and artworks.</span>
+                </p>
+            </div>
         </div>
     )
 }
+
 
 export default Home;
